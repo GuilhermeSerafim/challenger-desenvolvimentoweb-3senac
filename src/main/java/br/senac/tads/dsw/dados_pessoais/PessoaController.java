56bdc5a92e4353282f37eba32781242323f49ac4
GuilhermeSerafim <guilerstudies@gmail.com>
2025-09-16 21:21:40 -0300

    Get with dynamic param

--- src/main/java/br/senac/tads/dsw/dados_pessoais/PessoaController.java
+++ src/main/java/br/senac/tads/dsw/dados_pessoais/PessoaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +25,11 @@ public class PessoaController {
     @GetMapping("/apelido")
     public PessoaDTO findByUsernameRequestParam(@RequestParam(value = "username", required = false) String userNameIN) {
         if(userNameIN == null) service.findByUserName("fulano");
+        return service.findByUserName(userNameIN);
+    }
+    
+    @GetMapping("/{username}")
+    public PessoaDTO findByUsernameRequest(@PathVariable("value") String userNameIN) {
         return service.findByUserName(userNameIN);
     }
 }
