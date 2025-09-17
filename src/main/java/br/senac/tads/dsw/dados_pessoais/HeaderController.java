a71e12f0956a6ded4b1c13f9c53172a0c097f89a
GuilhermeSerafim <guilerstudies@gmail.com>
2025-09-16 21:25:02 -0300

    Get header params

---
+++ src/main/java/br/senac/tads/dsw/dados_pessoais/HeaderController.java
@@ -0,0 +1,15 @@
+package br.senac.tads.dsw.dados_pessoais;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HeaderController {
+    @GetMapping("/headers")
+    public Map<String, Object> obterTodosHeaders(@RequestHeader Map<String, Object> headers) {
+        return headers;
+    }
+}
