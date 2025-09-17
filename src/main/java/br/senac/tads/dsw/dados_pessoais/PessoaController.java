2aad1eef68fac7b35629b1925eb4086f102dfe33
GuilhermeSerafim <guilerstudies@gmail.com>
2025-09-16 22:22:41 -0300

    add new people with post - created URI to response 201

--- src/main/java/br/senac/tads/dsw/dados_pessoais/PessoaController.java
+++ src/main/java/br/senac/tads/dsw/dados_pessoais/PessoaController.java
@@ -1,14 +1,20 @@
 package br.senac.tads.dsw.dados_pessoais;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class PessoaController {
@@ -36,4 +42,17 @@ public class PessoaController {
         if(p == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pesquisa %s não encontrada".formatted(userNameIN));
         return p;
     }
+
+
+    @PostMapping()
+    public ResponseEntity<?> addNew(@RequestBody PessoaDTO p) {
+        PessoaDTO pessoa = service.addNew(p);
+        URI location = ServletUriComponentsBuilder
+            .fromCurrentRequestUri()
+            .path("/{username}")
+            .buildAndExpand(pessoa.getUsername())
+            .toUri();
+        return ResponseEntity.created(location).build();
+    }
+    
 }
