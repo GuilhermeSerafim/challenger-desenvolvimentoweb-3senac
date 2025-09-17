39a14813c5f9d8b572ce14a16a3380c0427d5b09
GuilhermeSerafim <guilerstudies@gmail.com>
2025-09-16 21:51:19 -0300

    Retornando erro not found quando não encontramos o elemento de uma forma mais 'manual'

--- src/main/java/br/senac/tads/dsw/dados_pessoais/PessoaController.java
+++ src/main/java/br/senac/tads/dsw/dados_pessoais/PessoaController.java
@@ -3,10 +3,12 @@ package br.senac.tads.dsw.dados_pessoais;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class PessoaController {
@@ -29,7 +31,9 @@ public class PessoaController {
     }
     
     @GetMapping("/{username}")
-    public PessoaDTO findByUsernameRequest(@PathVariable("value") String userNameIN) {
-        return service.findByUserName(userNameIN);
+    public PessoaDTO findByUsernameRequest(@PathVariable("username") String userNameIN) {
+        PessoaDTO p = service.findByUserName(userNameIN);
+        if(p == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pesquisa %s não encontrada".formatted(userNameIN));
+        return p;
     }
 }
